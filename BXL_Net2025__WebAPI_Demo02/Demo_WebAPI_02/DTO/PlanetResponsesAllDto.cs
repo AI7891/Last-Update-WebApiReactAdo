e57a2cee289fb7c5311603b74c7ebd190fe8e1ce
Aurelien St <aurelien.strimelle@bstorm.be>
2025-12-11 14:59:22 +0100

    Initialize demo 🐣

---
+++ BXL_Net2025__WebAPI_Demo02/Demo_WebAPI_02/DTO/PlanetResponsesAllDto.cs
@@ -0,0 +1,31 @@
+﻿namespace Demo_WebAPI_02.DTO
+{
+    public class PlanetResponsesAllDto
+    {
+        public required int Id { get; set; }
+        public required string Name { get; set; }
+    }
+    public class PlanetRequestDto 
+    {
+        public required string Name { get; set; }
+        public required int SolarSystemID { get; set; }
+        public required int No_Moon { get; set; }
+        
+        public required string? Description { get; set; }
+        public DateTime DiscoveryPlanetDate { get; set; }
+        
+        
+    }
+    public class PlanetResponseDto 
+    {
+        public required int Id { get; set; }
+        public required string Name { get; set; }
+        public required int SolarSystemID { get; set; }
+        public required int No_Moon { get; set; }
+        
+        public required string? Description { get; set; }
+        public DateTime DiscoveryPlanetDate { get; set; }
+        
+        
+    }
+}
